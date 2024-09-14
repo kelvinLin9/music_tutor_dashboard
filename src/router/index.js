a07dbaf97ad184968cdc32def7eafa67e07d045d
kelvin <kelvin80121@gmail.com>
2024-09-14 22:19:19 +0800

    refactor: Update router history configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/music_tutor_dashboard/'),
+  history: createWebHashHistory('/music_tutor_dashboard/'),
   routes: [
     {
       path: '/',
